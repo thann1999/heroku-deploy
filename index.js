f68a326b8fe6c04f712a6189654e06a9def2bc92
Thann <nguyenthanhthang185@gmail.com>
2021-06-04 02:19:35 +0700

    new commit

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ app.use('/api/file', fileRouter);
 app.use('/api/comment', commentRouter);
 app.use('/api/common', commonRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
@@ -48,14 +48,14 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.send(res.locals.message);
-});
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.send(res.locals.message);
+// });
 
 var port = process.env.PORT || '3000';
 app.listen(port, () => {
