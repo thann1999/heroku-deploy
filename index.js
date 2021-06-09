a8efd04c8462c6f7b174bc31334fe0b377d3b460
THANG Nguyen Thanh <thang.nguyenthanh@vti.vn>
2021-06-09 18:51:53 +0700

    new commit

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ app.use(function (req, res, next) {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  console.log('production');
   app.use(express.static(path.join(__dirname, 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
@@ -58,7 +57,7 @@ app.use(function (err, req, res, next) {
   res.send(res.locals.message);
 });
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
 });
