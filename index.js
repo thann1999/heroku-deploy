567effe28e2be12d83701da791a4b1e40c15ddbd
THANG Nguyen Thanh <thang.nguyenthanh@vti.vn>
2021-06-09 19:19:49 +0700

    new commit

--- index.js
+++ index.js
@@ -42,9 +42,11 @@ app.use(function (req, res, next) {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  // Step 1:
+  app.use(express.static(path.resolve(__dirname, './client/build')));
+  // Step 2:
+  app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
   });
 }
 // error handler
