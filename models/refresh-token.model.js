c8dc4c0553f54beca955ca62b5d312f9c3e064ec
Thann <nguyenthanhthang185@gmail.com>
2021-06-22 22:46:28 +0700

    new commit

--- models/refresh-token.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const refreshTokenSchema = new mongoose.Schema({
-  token: {type: String, required: true},
-  createdDate: { type: Date, default: Date.now, required: true },
-});
-
-module.exports = mongoose.model("RefreshToken", refreshTokenSchema);
