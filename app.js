9f551f62d09c8c134894c6c00df1b43ac4fdd38f
Thann <nguyenthanhthang185@gmail.com>
2021-06-22 23:23:42 +0700

    new commit

--- app.js
+++ app.js
@@ -52,10 +52,10 @@ app.use(function (err, req, res, next) {
   res.send(res.locals.message);
 });
 
-app.use(express.static(path.join(__dirname, 'client/build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build'))
-})
+app.use(express.static(path.resolve(__dirname, "client/build")));
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
